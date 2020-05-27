ecedf98386ebc26b5198489d0f8579202d3e863b
thefireblade <jason43341@gmail.com>
2020-05-27 19:27:58 -0400

    Pushing updates to the control panel

--- public/js/controlpanel.js
+++ public/js/controlpanel.js
@@ -5,6 +5,21 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
 
+//////////////////////////////// Helper Functions /////////////////////////////////////////////////
+/**
+ * An empty indice is an element with value ''
+ */
+function removeEmptyIndices(array){
+    array.forEach(a => {
+        // Not the best solution but accounts for changing indices
+        array.forEach(b => {
+            if(b == ""){
+                array.splice(array.indexOf(b),1);
+            }
+        });
+    });
+    return array;
+}
 //////////////////////////////// ACTIVITY FUNCTIONS //////////////////////////////////////////////
 const editor = "";
 /**
@@ -59,43 +74,148 @@ function removeActivity(id) {
         });
     });
 }
+function updateActivity(id){
+    let skillsTable = $("#skills").DataTable().rows().data();
+    let checkTable = $('#checklist').DataTable().rows().data();
+    let skills = [];
+    let checklist = [];
+    let activityName = document.getElementById("act-name").value;
+    /////////////////////// update to checklist ///////////////////////////////
+    for(let i = 0; i < checkTable.length; i++) {
+        let id = checkTable[i][5];
+        let checkName = document.getElementById("check-" + id).value;
+        let checkUnit = document.getElementById("check-unit-" + id).value;
+        checklist.push({name:checkName, type:checkUnit});
+    }
+    /////////////////////// update to skills ///////////////////////////////
+    for(let i = 0; i < skillsTable.length; i++){
+        let id = skillsTable[i][5];
+        let skillName = document.getElementById("skill-" + id).value;
+        let subSkillStr = document.getElementById("subskill-" + id).value;
+        let subSkills = subSkillStr.split(",");
+        subSkills = removeEmptyIndices(subSkills);
+        skills.push({
+            skillName: skillName,
+            subSkills: subSkills
+        });
+    }
+    console.log(JSON.stringify({checklist: checklist, name: activityName, skills: skills}));
+}
 function getActivity(id) {
     fs.collection('Activities').doc(id).get().then(doc =>{
         document.getElementById('act-name').value = doc.data()['name'];
         document.getElementById('act-edit').style = "display: block";
+        ///////////////////////////////////////     init checkList Table /////////////////////////////////////////
+        let checklist = []
+        doc.data()['checklist'].forEach((checkItem) => {
+            let id = checkItem['name'] + Math.random().toString(36).substring(2, 8);
+            let temp = [
+                `<input type="text" id="${"check-" + id}" class="input" value="${checkItem['name']}">`,
+                `<input type="text" id="${"check-unit-" + id}" class="input" value="${checkItem['type']}">`,
+                `<button class='btn bdrlessBtn' onclick='removeCheck("${id}")'>Remove</button>`,
+                checkItem['name'], checkItem['type'], id
+            ];
+            checklist.push(temp);
+        });
+        ///////////////////////////////////////     init Skills Table  //////////////////////////////////////////
         let skills = []
         doc.data()['skills'].forEach((skill) => {
-            let temp = {}
-            temp['name'] = `<input type="text" class="input" value="${skill['name']}">`;
             let subSkillsStr = "";
             skill['subSkills'].forEach((subSkill)=> {
-                subSkillsStr += ' "' + subSkill + '"';
+                subSkillsStr += '' + subSkill + ",";
             });
-            temp['subSkills'] = `<input type="text" class="input" value=${subSkillsStr}">`;
+            let id = skill['skillName'] + Math.random().toString(36).substring(2, 8);
+            let temp = [
+                `<input type="text" id="${"skill-" + id}" class="input" value="${skill['skillName']}">`,
+                `<input type="text" id="${"subskill-" + id}" class="input" value="${subSkillsStr}">`,
+                `<button class='btn bdrlessBtn' onclick='removeSkill("${id}")'>Remove</button>`,
+                skill['skillName'], subSkillsStr, id
+            ];
             skills.push(temp);
         });
         $(document).ready(function() {
-            if($('#skills').DataTable()) {
-                $('#skills').DataTable().destroy();
+            ////////////////// add data to checklist table //////////////////////////
+            if(!$('#checklist').DataTable()){
+                $('#checklist').DataTable({  
+                    columns: [
+                        {"title" : "Checklist Name", 'searchable': false},
+                        {"title" : "Unit of Measure", 'searchable': false},
+                        {"title" : "", 'searchable': false},
+                        {"title": "", 'visible' : false},
+                        {"title": "", 'visible' : false},
+                        {"title": "", 'visible' : false,'searchable': false}
+                    ]
+                });
             }
-            $('#skills').DataTable({  
-                data: skills,
-                columns: [
-                    {"data" : "name"},
-                    {"data" : "subSkills"},
-
-                ]
-            });
+            $('#checklist').DataTable().clear();
+            $('#checklist').DataTable().rows.add(checklist).draw();
+            ////////////////// add data to Skills Table ////////////////////////////
+            if(!$('#skills').DataTable()) {
+                $('#skills').DataTable({  
+                    columns: [
+                        {"title" : "Skill Name", 'searchable': false},
+                        {"title" : "Subskills", 'searchable': false},
+                        {"title" : "", 'searchable': false},
+                        {"title": "", 'visible' : false},
+                        {"title": "", 'visible' : false},
+                        {"title": "", 'visible' : false,'searchable': false}
+                    ]
+                });
+            }
+            $('#skills').DataTable().clear();
+            $('#skills').DataTable().rows.add(skills).draw();
+            document.getElementById("update-activity").onclick = function(){updateActivity(id)};
         });
     }).catch(err => {console.log(err);});
 }
+function addSkill(){
+    let id = Math.random().toString(36).substring(2, 8) + Math.random().toString(36).substring(2, 8);
+    let tempSkill = [
+        `<input type="text" id="${"skill-" + id}" class="input" value="Skill">`, 
+        `<input type="text" id="${"subskill-" + id}" class="input" value="Subskill, Subskill">`,
+        `<button class='btn bdrlessBtn' onclick='removeSkill("${id}")'>Remove</button>`,
+        "Skill", "Subskill, Subskill", id
+    ]
+    $('#skills').DataTable().row.add(tempSkill).draw();
+}
+function removeSkill(id){
+    let skillTable = $('#skills').DataTable().rows().data();
+    for(let i = 0; i < skillTable.length; i++) {
+        let skillID = skillTable[i][5];
+        if(skillID == id) {
+            skillTable.row(i).remove().draw();
+            break;
+        }
+    }
+}
+function addCheckList(){
+    let id = Math.random().toString(36).substring(2, 8) + Math.random().toString(36).substring(2, 8);
+    let checklist = [
+        `<input type="text" id="${"check-" + id}" class="input" value="Example Checklist name">`, 
+        `<input type="text" id="${"check-unit-" + id}" class="input" value="inches">`,
+        `<button class='btn bdrlessBtn' onclick='removeCheck("${id}")'>Remove</button>`,
+        "Example Checklist name", "inches", id
+    ]
+    $('#checklist').DataTable().row.add(checklist).draw();
+}
+function removeCheck(id){
+    let checkTable = $('#checklist').DataTable().rows().data();
+    for(let i = 0; i < checkTable.length; i++) {
+        let checkID = checkTable[i][5];
+        if(checkID == id) {
+            checkTable.row(i).remove().draw();
+            break;
+        }
+    }
+}
 function addActivity(){
     let data = {
+        checklist: [{name: "example check list item", type: "unit of measurement"}],
         name: "Example Activity",
         skills: [
             {
                 name: "Example Skill",
-                subSkills: ["subSkill Example"]
+                subSkills: ["subSkill Example", "Another subskill"]
             }
         ]
     };
@@ -121,7 +241,7 @@ function initCampersTable(){
             data.push([doc.data()['firstName'], doc.data()['lastName'], doc.data()['id'],
             `<button class='btn bdrlessBtn' onclick='getCamper("${doc.id}")'>Edit</button>`,
             `<button class='btn bdrlessBtn btn-danger' onclick='removeCamper("${doc.id}")'>Remove</button>`
-        ]);
+            ]);
         });
         $(document).ready(function() {
             $('#campers').DataTable({        
@@ -214,8 +334,8 @@ function initGroupsTable(){
                         $('#groups').DataTable().row.add([
                         tempSelect.outerHTML, 
                         `<textarea class="form-control" id="${"group-" + doc.id}" rows="1"></textarea>`,
-                        `<button class='btn bdrlessBtn btn-danger' onclick='removeGroup("${doc.id}")'>Remove</button>`,
                         `<button class='btn bdrlessBtn' onclick='updateGroup("${doc.id}")'>Update</button>`,
+                        `<button class='btn bdrlessBtn btn-danger' onclick='removeGroup("${doc.id}")'>Remove</button>`,
                         coaches[doc.data()['coach']]['firstName'] + " " + coaches[doc.data()['coach']]['lastName'] + `(id:${doc.data()['coach']})`,
                         camperNames
                         ]).draw(); 
@@ -268,18 +388,11 @@ function updateGroup(docid) {
     var re = new RegExp(find, 'g');
     camperString = camperString.replace(re,"").trim();
     let campers = camperString.split(" ");
-    campers.forEach(c => {
-        campers.forEach(camper=> {
-            if(camper == ""){
-                campers.splice(campers.indexOf(camper),1);
-            }
-        });
-    });
+    campers = removeEmptyIndices(campers);
     let data = {
         campers: campers,
         coach: document.getElementById("group-select-" + docid).value
     };
-    // console.log(data);
     fs.collection("Groups").doc(docid).set(data);
 }
 function initUsersPage() {
