884153f2cc988770bd3012c7a5e89d88e98eaf91
Britney Echeverria <britney@Britneys-Air.home>
2020-08-12 18:27:55 -0400

    add offline functionality, update service worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v7';
-const dynamicCacheName = 'site-dynamic-v5';
+const staticCacheName = 'site-static-v7.4';
+const dynamicCacheName = 'site-dynamic-v5.5';
 // // TODO: Add firebase js files in assets locally and locally to index.html
 const assets = [
     '/',
@@ -14,16 +14,15 @@ const assets = [
     '/js/jquery-3.4.1.min.js',
     '/style.css',
     '/css/app.css',
-    // '/css/bluetooth.css',
+    '/css/bluetooth.css',
     '/css/login.css',
     '/css/menu.css',
     '/css/bootstrap.min.css',
     '/img/logobackground.png',
-    // '/templates/About.html',
-    // '/templates/Contact.html',
+    '/templates/About.html',
+    '/templates/Contact.html',
     '/templates/Home.html',
-    // '/templates/Product.html',
-    //
+    '/templates/Product.html',
     '/js/bluetooth.js',
     '/js/controlpanel.js',
     '/js/evaluation.js',
@@ -34,27 +33,28 @@ const assets = [
     '/css/home.css',
     '/css/jquery.dataTables.min.css',
     '/css/navbar.css',
-    // '/css/selectr.min.css',
+    '/css/selectr.min.css',
     '/css/table.css',
-    // '/templates/ControlPanel.html',
-    // '/templates/EditProfile.html',
-    // '/templates/Evaluation.html',
+    '/templates/ControlPanel.html',
+    '/templates/EditProfile.html',
+    '/templates/Evaluation.html',
     '/templates/ForgotPassword.html',
     '/templates/Login.html',
-    // '/templates/MissingInfo.html',
-    // '/templates/Navbar.html',
-    // '/templates/UserDetails.html'
+    '/templates/MissingInfo.html',
+    '/templates/Navbar.html',
+    '/templates/UserDetails.html'
 ];
 
-// const limitCacheSize = (name, size) => {
-//     caches.open(name).then(cache => {
-//         cache.keys().then(keys => {
-//             if(keys.length > size){
-//                 cache.delete(keys[0]).then(limitCacheSize(name, size));
-//             }
-//         });
-//     });
-// };
+// Limit Dynamic Cache Size
+const limitCacheSize = (name, size) => {
+    caches.open(name).then(cache => {
+        cache.keys().then(keys => {
+            if(keys.length > size){
+                cache.delete(keys[0]).then(limitCacheSize(name, size));
+            }
+        });
+    });
+};
 
 // Install Event
 self.addEventListener('install', evt => {
@@ -62,7 +62,9 @@ self.addEventListener('install', evt => {
     evt.waitUntil(
         caches.open(staticCacheName).then((cache) => {
             console.log('Caching Assets');
-            cache.addAll(assets);
+            cache.addAll(assets.map(function(asset){
+                return new Request(asset, { mode: 'no-cors' });
+            }));
         })
     );
 });
@@ -84,13 +86,17 @@ self.addEventListener('activate', evt => {
 // Fetch
 self.addEventListener('fetch', evt => {
     //console.log('Fetch Event', evt);
-    evt.respondWith(
-        caches.match(evt.request).then(cacheRes => {
-            return cacheRes || fetch(evt.request).then(fetchRes => {
-                return caches.open(dynamicCacheName).then(cache => {
-                    cache.put(evt.request.url, fetchRes.clone());
-                    return fetchRes;
-                })
-            });
-        }));
+    if(evt.request.url.indexOf('firestore.googleapis.com') === -1){
+        evt.respondWith(
+            caches.match(evt.request).then(cacheRes => {
+                return cacheRes || fetch(evt.request).then(fetchRes => {
+                    return caches.open(dynamicCacheName).then(cache => {
+                        cache.put(evt.request.url, fetchRes.clone());
+                        limitCacheSize(dynamicCacheName, 300);
+                        return fetchRes;
+                    })
+                });
+            }));
+    }
+    
 });
