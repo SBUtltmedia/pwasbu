dfc46839268308d976294910dfb1a5951e443d2b
Pratik Gurung <pratik.gurung@stonybrook.edu>
2020-11-08 22:32:12 -0500

    Updated static and dynamic cache versions for new deployment

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-v7.4';
-const dynamicCacheName = 'site-dynamic-v5.5';
+const staticCacheName = 'site-static-v7.5';
+const dynamicCacheName = 'site-dynamic-v7.5';
 // // TODO: Add firebase js files in assets locally and locally to index.html
 const assets = [
     '/',
